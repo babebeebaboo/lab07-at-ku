00a44f661e1529f3cefa48e6d240323c281e2dfa
Supakorn Wongsawang <supakorn.wo@ku.th>
2016-10-18 21:42:17 +0700

    Update cuteCube.cs

--- cuteCube.cs
+++ cuteCube.cs
@@ -4,14 +4,10 @@
 //PROGRAM: cuteCube.cs                       
 //DATE: 13 Oct 2016                          
 /////////////////////////////////////////////
-﻿
 using System;
-
-namespace lab07
-{
-    class Program
-    {
-    	static void Main()
+namespace lab07{
+    class Program{
+        static void Main()
         {
             Console.Write("N: ");
             int n;
@@ -19,51 +15,45 @@ namespace lab07
             while (true)
             {
                 if (Int32.TryParse(nn, out n)) break;
-                else 
-					 {
-					 	Console.Write("N: ");
-					 	nn = Console.ReadLine();
-					 }
+                else
+                {
+                    Console.Write("N: ");
+                    nn = Console.ReadLine();
+                }
             }
-            int ntimn=n*n;
+            int ntimn = n * n, pat;
             Console.Write("PATTERN: ");
-            int pat;
             nn = Console.ReadLine();
             while (true)
             {
                 if (Int32.TryParse(nn, out pat)) break;
-                else 
-					 {
-					 	Console.Write("PATTERN: ");
-					 	nn = Console.ReadLine();
-					 }
+                else
+                {
+                    Console.Write("PATTERN: ");
+                    nn = Console.ReadLine();
+                }
             }
             if (pat == 1)
-            {
                 for (int i = 0; i < n; i++)
                 {
                     for (int j = 0; j < n; j++) Console.Write("{0:00} ", j + 1 + (i * n));
                     Console.WriteLine();
                 }
-            }
             if (pat == 2)
-            {
                 for (int i = 0; i < n; i++)
                 {
                     for (int j = 0; j < n; j++) Console.Write("{0:00} ", i + 1 + (j * n));
                     Console.WriteLine();
                 }
-            }
-            int odd = 1;
-            int even = 2;
+            int odd = 1,even = 2;
             if (pat == 3)
             {
                 if (n % 2 == 0)
                 {
-                    for(int i = 1; i <= n; i++)
+                    for (int i = 1; i <= n; i++)
                     {
                         if (i % 2 == 1)
-                            for(int j = 1; j <= n; j++)
+                            for (int j = 1; j <= n; j++)
                             {
                                 Console.Write("{0:00} ", odd);
                                 odd += 2;
@@ -79,7 +69,7 @@ namespace lab07
                 }
                 else
                 {
-                    for (int i = 1; i <= n-1; i++)
+                    for (int i = 1; i <= n - 1; i++)
                     {
                         if (i % 2 == 1)
                             for (int j = 1; j <= n; j++)
@@ -108,17 +98,14 @@ namespace lab07
                             even += 2;
                         }
                     }
-
                 }
             }
             if (pat == 4)
-            {
-                for (int i = n-1; i >=0; i--)
+                for (int i = n - 1; i >= 0; i--)
                 {
                     for (int j = 0; j < n; j++) Console.Write("{0:00} ", j + 1 + (i * n));
                     Console.WriteLine();
                 }
-            }
         }
     }
 }
